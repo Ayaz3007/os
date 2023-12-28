b07341b882b3a198723ff3ad78734b37910d5c74
Ayaz3007 <43346430+Ayaz3007@users.noreply.github.com>
2023-12-28 21:50:40 +0300

    Add files via upload

---
+++ producer.py
@@ -0,0 +1,18 @@
+import random
+import time
+import sys
+import os
+
+N = random.randint(120, 180)
+
+for _ in range(N):
+    X = random.randint(1, 9)
+    O = random.choice(['+', '-', '*', '/'])
+    Y = random.randint(1, 9)
+
+    print(f"{X} {O} {Y}")
+    sys.stdout.flush()
+
+    time.sleep(1)
+
+os._exit(0)
